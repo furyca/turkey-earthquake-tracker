6b837f846c870f4b5b7495442d3f8005dbc5eab9
Furkan <ilfuryca@gmail.com>
2023-03-05 06:04:42 +0300

    Add Filter Functionality
    Filter by magnitude functionality added.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Map from "./components/Map";
 function App() {
   const [quakeList, setQuakeList] = useState([])
   const [loading, setLoading] = useState(false)
+  const [magLimits, setMagLimits] = useState({
+    minLimit: 0,
+    maxLimit: 10
+  })
 
   useEffect (() => {
     const fetchData = async () => {
@@ -16,13 +20,21 @@ function App() {
       setQuakeList(data.result)
       setLoading(false)
     }
+
     fetchData()
   }, [])
 
+  const handleMinMag = (minMag) => {
+    minMag > magLimits.maxLimit ? setMagLimits({minLimit: minMag, maxLimit: minMag}) : setMagLimits({...magLimits, minLimit: minMag})
+  }
+  const handleMaxMag = (maxMag) => {
+    maxMag < magLimits.minLimit ? setMagLimits({maxLimit: maxMag, minLimit: maxMag}) : setMagLimits({...magLimits, maxLimit: maxMag})
+  }
+
   return (
     <div>
-      <Header />
-      { !loading ? <Map quakeList={quakeList} /> : <Loader />} 
+      <Header handleMinChange={handleMinMag} handleMaxChange={handleMaxMag} />
+      { loading ? <Loader /> : <Map quakeList={quakeList} minMagnitude={magLimits.minLimit} maxMagnitude={magLimits.maxLimit} />} 
     </div>
   );
 }
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -6,47 +6,21 @@ import LocationInfoBox from './LocationInfoBox';
 
 const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
 
-/*
-	"title": "YENIKOY ACIKLARI-TEKIRDAG (MARMARA DENIZI)", -> kandilli deprem başlığı
-	"date": "2023.02.13 06:33:53", -> kandilli deprem zaman damgası
-	"mag": 3, -> Deprem büyüklüğü
-	"depth": 14.8, -> Depremin kaç km yerin derinliğinde olduğu
-	"geojson": { -> GeoJson Datası
-		"type": "Point",
-		"coordinates": [27.4988, 40.77]
-	},
-	"location_properties": {
-		"closestCity": { 
-			"name": "Tekirdağ" -> En yakın il sınırı
-		},
-		"epiCenter": {
-			"name": "?" -> Depremin epicenterı
-		},
-	},
-	"rev": null, -> Kandilli revize bilgisi
-	"date_stamp": "2023-02-13",
-	"date_day": "2023-02-13",
-	"date_hour": "06:33:53",
-	"timestamp": "1676266433",
-	"location_tz": "Europe/Istanbul"
-
-*/
-
 const center = {
   lat: 38.75,
   lng: 36
 };
-const Map = ( {quakeList} ) => {
+const Map = ( {quakeList, minMagnitude, maxMagnitude} ) => {
   const [locationInfo, setLocationInfo] = useState(null)
 
-
   const markers = quakeList.map(quake => {
-    return <LocationIcon 
+    return quake.mag >= minMagnitude && quake.mag <= maxMagnitude && 
+    <LocationIcon 
       key={uuid()}
       lat={quake.geojson.coordinates[1]}
       lng={quake.geojson.coordinates[0]}
       onClick={() => setLocationInfo({quake})}
-      />
+    />
   })
 
   return (
